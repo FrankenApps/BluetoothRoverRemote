387ffac9811e5386acd36df9d977e4a21c7ca219
FrankenApps <de.frankenapps@gmail.com>
2016-05-18 11:29:19 +0200

    Added Files

---
+++ app/src/main/java/com/example/android/BluetoothChat/Info.java
@@ -0,0 +1,16 @@
+package com.example.android.BluetoothChat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Info extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
